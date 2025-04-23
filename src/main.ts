50d7e9103809c2a382d19dd69ab8859d5dac9caa
William Alejandro C.m <crazzy.wili1990@hotmail.com>
2025-04-23 13:42:43 -0500

    Se habilito el CORS para pruebas

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // o el puerto donde corre tu frontend
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
